dd249d1eee731ea49e30e68c8acb34c70356473f
doabit <doinsist@gmail.com>
2012-07-25 19:30:04 +0800

    with default value

--- app/models/mongoid/auto_increment.rb
+++ app/models/mongoid/auto_increment.rb
@@ -3,12 +3,12 @@ module Mongoid
     extend ActiveSupport::Concern
 
       included do
-        field :_id,  :type => Integer
+        field :_id,  :type => Integer, default: ->{ inc_id }
         before_create :inc_id
       end
 
       def inc_id
-        self.id = Mongoid.default_session.command(findAndModify: "mongoid_autoinc_id",
+        Mongoid.default_session.command(findAndModify: "mongoid_autoinc_id",
                   query: {_id:self.class.name},
                   update: {"$inc" => {next:1}},
                   new: true,
